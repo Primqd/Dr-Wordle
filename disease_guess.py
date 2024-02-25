bb2d242480007c8409cf026919db69eed4a438f7
Primqd <90920108+Primqd@users.noreply.github.com>
2024-02-25 15:29:04 -0800

    The thing
    The actual files for the web app. lol

---
+++ disease_guess.py
@@ -0,0 +1,163 @@
+from openai import OpenAI
+from random import randint
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+diseases = [
+    "COVID-19",
+    "Alzheimer’s disease",
+    "Diabetes I",
+	"Diabetes II",
+    "Influenza",
+	"Pneumonia",
+    "Kidney Cancer",
+    "Liver Cancer",
+    # "Hypertension",
+    # "Obesity",
+    # "Drug overdose",
+    "Parkinson’s disease",
+    "HIV/AIDS",
+    # "Liver cirrhosis",
+    # "Osteoarthritis",
+    # "Rheumatoid arthritis",
+    "Depression",
+    # "Gastroesophageal reflux disease",
+    "Asthma",
+    # "COPD",
+    "Epilepsy",
+    "Breast cancer",
+    # "Prostate cancer",
+    # "Colon cancer",
+    # "Pancreatic cancer",
+    "Leukemia",
+    "Lung cancer",
+    "Skin cancer",
+    # "Hepatitis",
+    # "Glaucoma",
+    # "Cataracts",
+    "Dementia",
+    # "Multiple sclerosis",
+    # "Endometriosis",
+    # "Sickle cell anemia",
+    # "Celiac disease",
+    # "Crohn’s disease",
+    # "Ulcerative colitis",
+    # "Lupus",
+    # "Fibromyalgia",
+    # "Sleep apnea",
+    "Osteoporosis",
+    "Hemorrhoids",
+    # "Gout",
+    # "Psoriasis",
+    "Tuberculosis",
+    # "Lyme disease",
+    # "Chlamydia",
+    # "Gonorrhea",
+    "Syphilis",
+    "Herpes",
+    # "Human papillomavirus",
+    "Measles",
+    # "Mumps",
+    # "Rubella",
+    "Varicella (chickenpox)",
+    "Zika virus",
+    # "West Nile virus",
+    "Ebola",
+    "Malaria",
+    "Yellow fever",
+    "Cholera",
+    # "Typhoid fever",
+    # "Dengue fever",
+    # "Lassa fever",
+    # "Hantavirus",
+    "Rabies",
+    "Tetanus",
+    # "Botulism",
+    # "Anthrax",
+    # "Black Plague",
+    # "Smallpox",
+    # "Polio",
+    # "Norovirus",
+    # "Salmonella",
+    # "E. coli infection",
+    # "Listeria",
+    # "Campylobacter",
+    # "Toxoplasmosis",
+    # "Cryptosporidiosis",
+    # "Giardiasis",
+    # "Trichomoniasis",
+    # "Scabies",
+    "Head Lice",
+    # "Ringworm infestation",
+    # "Pinworm infection",
+    "Bedbugs",
+    # "MRSA (methicillin-resistant Staphylococcus aureus)",
+    # "Clostridioides difficile infection",
+    # "Legionnaires’ disease",
+    "Tuberculosis",
+	"Hepatitis",
+    "Syphilis",
+    # "Gonorrhea",
+    "Chlamydia"
+    # "Human papillomavirus (HPV)",
+    # "Herpes simplex virus (HSV)",
+    # "Trichomoniasis",
+    # "Pelvic inflammatory disease (PID)",
+    # "Genital warts",
+    # "Genital herpes",
+    # "Chancroid",
+    # "Lymphogranuloma venereum (LGV)",
+    # "Granuloma inguinale",
+    # "Molluscum contagiosum",
+]
+
+def getDisease():
+	return diseases[randint(0, len(diseases) - 1)]
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+def reload():
+    global disease, patient
+    disease = getDisease()
+
+disease = getDisease()
+
+thread = client.beta.threads.create()
+
+def guess(tried):
+    """
+    The user guessed a disease.
+    """
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo-0125",
+    messages=[
+        {"role": "user", "content": f"Return a common characteristic shared between {disease} and {tried} without mentioning the word {disease}. If {tried} and {disease} are the same, print the statement 'True' instead. If {tried} is not a disease, then print the statement 'False' instead."}
+    ]
+    )
+    return (response.choices[0].message.content)
+
+def hint():
+    """
+    The user wants a hint
+    """
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo-0125",
+    messages=[
+        {"role": "user", "content": f"Return a defining characteristic of {disease}. Do not include the word {disease} in your response."}
+    ]
+    )
+    return (response.choices[0].message.content)
+
+
+def give_up(): return disease
+
+def fun_fact():
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo-0125",
+    messages=[
+        {"role": "user", "content": f"Give me two sentences about {disease}; one about the symptoms, one about how to prevent the disease."}
+    ]
+    )
+    return (response.choices[0].message.content)
---
+++ main.py
@@ -0,0 +1,52 @@
+import streamlit as st
+from disease_guess import hint, guess, give_up, fun_fact, reload
+from streamlit_js_eval import streamlit_js_eval
+curr = st.session_state
+
+def good(k, k_response = ""):
+    if k == "True":
+        return "You got the word!"
+    elif k == "False":
+        return "Not a valid response, try again"
+    else:
+        return k_response
+if 'hint' not in curr:
+    curr.hint = hint()
+if 'previous' not in curr:
+    curr.previous = []
+
+image = st.image("diseasewordle.jpg")
+
+st.markdown("<h5 style='text-align: center; color: white;'>Guess a hidden disease by typing in the box. Dr. Wordle will return shared features with the hidden disease.</h5>", unsafe_allow_html=True)
+
+guessed = st.text_input("Guess Here:")
+if guessed:
+    response = guess(guessed)
+    if response == "True":
+        st.write("You got the word! " + fun_fact())
+    elif response == "False": st.write("Not a valid response")
+    else:
+        st.markdown(f"**{response}**")
+        curr.previous.append((guessed, good(guessed, response)))
+else:
+    st.write("No input detected. Try again.")
+
+st.divider()
+st.write("\n")
+st.write("Previous Guesses:")
+
+for _ in range(-1, -len(curr.previous) - 1, -1):
+    i = curr.previous[_]
+    st.write(f"{i[0]}: **{i[1]}**")
+
+st.divider()
+st.write("\n")
+if st.button('Hint'):
+    st.write(f'Hint: {curr.hint}')
+if st.button('Give Up'):
+    st.write(f'You gave up. The answer is {give_up()}.')
+if st.button("New Puzzle"):
+    reload()
+    streamlit_js_eval(js_expressions="parent.window.location.reload()")
+st.write("Created as part of WAForge")
+st.write("By Franklin Chen, Varun Patankar, Karunya Penumalla")
